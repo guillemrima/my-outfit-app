374d3fd99ea2332f84e0f018680490b81e1de7d4
guillemrima <117720260+guillemrima@users.noreply.github.com>
2023-05-15 23:12:43 +0200

    Feat: Start express server

---
+++ api/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const cors = require('cors');
+const fs = require('fs');
+const multer = require('multer');
+const app = express();
+
+
+var corsOptions = {
+    origin: ['http://localhost:8081', 'http://127.0.0.1:5500', 'http://127.0.0.1:5501']
+}
+
+app.use(cors(corsOptions));
+app.use(express.json({limit:'10mb', extended: true}))
+app.use(express.urlencoded({limit: "10mb", extended: true, parameterLimit: 50000}));
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'src/storage/tmp/')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname)
+    }
+});
+
+const upload = multer({ storage: storage })
+
+// var routePath = './src/routes';
+
+// fs.readFileSync(routePath).forEach(function(file) {
+//     require(routePath + file)(app, upload)
+// })
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(`✔️  El servidor se ha iniciado correctamente en el puerto ${PORT}`)
+})
+
+
+
+
---
+++ api/src/routes/api-register-routes.js
@@ -0,0 +1,20 @@
+const { QueryError} = require('sequelize');
+
+module.exports = (app, upload) => {
+    const router = require('express').Router();
+    const controller = ''; //POSTERIORMENTE DECLARAR EL CONTROLADOR DE ESTE ENRUTADOR
+
+    app.use(function(req, res, next) {
+        res.header(
+            'Acces-Control-Allow-Headers',
+            'Authorization, Origin, Content-Type, Accept'
+        );
+        next();
+    });
+
+    router.get('/', () => {
+    })
+
+
+    app.use('/api/register', router)
+}    
